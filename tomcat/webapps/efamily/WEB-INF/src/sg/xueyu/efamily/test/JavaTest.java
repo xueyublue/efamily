cfd914febb5f27a0362afe12d3a9242e5c1bc8e7
Darren <xueyublue@gmail.com>
2017-09-29 13:41:02 +0800

    ignore a few folders

--- tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/test/JavaTest.java
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/test/JavaTest.java
@@ -1,11 +1,24 @@
 package sg.xueyu.efamily.test;
 
 import java.sql.SQLException;
+import java.text.ParseException;
+
+import sg.xueyu.dbhandler.handler.AbstractEntity;
+import sg.xueyu.efamily.base.NoPoolConnection;
+import sg.xueyu.efamily.base.dbhandler.LoginUser;
+import sg.xueyu.efamily.base.dbhandler.LoginUserHandler;
+import sg.xueyu.efamily.base.dbhandler.LoginUserSearchKey;
 
 public class JavaTest {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, InstantiationException,
+			IllegalAccessException, ParseException {
+
+		LoginUserHandler handler = new LoginUserHandler(NoPoolConnection.getConnection());
+		LoginUserSearchKey searchKey = new LoginUserSearchKey();
+		searchKey.setUserId("xueyu");
+		AbstractEntity[] results = handler.query(searchKey);
+		System.out.println(((LoginUser) results[0]).getUserName());
 	}
 
 }
