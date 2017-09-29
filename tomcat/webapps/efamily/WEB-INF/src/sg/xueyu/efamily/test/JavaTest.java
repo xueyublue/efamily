3cd0f1aa31d05d89aab0ec9065951215d45b8777
Darren <xueyublue@gmail.com>
2017-09-29 13:34:21 +0800

    add gitignore

--- tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/test/JavaTest.java
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/test/JavaTest.java
@@ -2,13 +2,10 @@ package sg.xueyu.efamily.test;
 
 import java.sql.SQLException;
 
-import sg.xueyu.efamily.system.EFamilyParam;
-
 public class JavaTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-		Use
 	}
 
 }
