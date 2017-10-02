62835622fcbe167362408893d93dca71e5734f87
Darren <xueyublue@gmail.com>
2017-10-02 10:10:34 +0800

    Add User model

---
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/action/UserAction.java
@@ -0,0 +1,24 @@
+package sg.xueyu.efamily.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sg.xueyu.zebra.action.Action;
+import sg.xueyu.zebra.action.ActionResult;
+import sg.xueyu.zebra.action.ResultContent;
+
+public class UserAction implements Action {
+
+	private static final String RESULT_URL = "user.jsp";
+	
+	@Override
+	public ActionResult execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		ResultContent resultContent = new ResultContent(RESULT_URL);
+		ActionResult result = new ActionResult(resultContent);
+		return result;
+	}
+
+}
