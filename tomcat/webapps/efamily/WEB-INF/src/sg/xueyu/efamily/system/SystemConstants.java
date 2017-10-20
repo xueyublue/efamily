3c09a7af8351f6d81de102a10012d7ec9efc33eb
WIN-Dell <xueyublue@gmail.com>
2017-10-20 17:00:14 +0800

    -

--- tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/system/SystemConstants.java
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/system/SystemConstants.java
@@ -7,4 +7,15 @@ public interface SystemConstants {
 	public static final String ROLE_ADMIN_FLAG_TRUE = "1";
 	public static final String ROLE_ADMIN_FLAG_FALSE = "0";
 	
+	// URLs
+	public static final String URL_INDEX = "login.jsp";
+	// Login
+	public static final String URL_LOGIN = "login.jsp";
+	// Home Tab
+	public static final String URL_HOME = "home.jsp";
+	// Family Share Tab
+	// Administrator Tab
+	public static final String URL_USER = "user.jsp";
+	public static final String URL_ROLE = "role.jsp";
+	
 }
