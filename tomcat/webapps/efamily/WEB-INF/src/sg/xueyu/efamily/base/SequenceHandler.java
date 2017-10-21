fe4085d3366d626cb10bffa705238111813b7b6b
MAC <xueyublue@gmail.com>
2017-10-21 20:32:40 +0800

    -

--- tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/base/SequenceHandler.java
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/base/SequenceHandler.java
@@ -3,10 +3,28 @@ package sg.xueyu.efamily.base;
 import java.sql.Connection;
 
 import sg.xueyu.dbhandler.handler.AbstractSequenceHandler;
+import sg.xueyu.efamily.system.CommonMethods;
 
 public class SequenceHandler extends AbstractSequenceHandler {
 
 	public static String nextEventId(Connection conn) throws Exception {
-		return next(conn, "SELECT EVENT_SEQ.NEXTVAL FROM DUAL");
+		String value = next(conn, "SELECT EVENT_SEQ.NEXTVAL FROM DUAL");
+		return addLeadingZeros(value, 10);
+	}
+
+/** Private Methods **/	
+	private static String addLeadingZeros(String value, int length) {
+		if (CommonMethods.isVoid(value) || length <= 0) {
+			return null;
+		} else if (value.length() >= length) {
+			return value;
+		} else {
+			int zeros = length - value.length();
+			String strZeros = "";
+			for (int i = 0; i < zeros; i++) {
+				strZeros += "0";
+			}
+			return strZeros + value;
+		}
 	}
 }
