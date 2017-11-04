1fc83c79b76b32867f896d901637dce6f00f453e
MAC <xueyublue@gmail.com>
2017-11-04 11:48:13 +0800

    -

---
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/action/BaseAction.java
@@ -0,0 +1,69 @@
+package sg.xueyu.efamily.action;
+
+import java.sql.Connection;
+
+import javax.servlet.http.HttpServletRequest;
+
+import sg.xueyu.efamily.base.DataSource;
+import sg.xueyu.efamily.base.SessionManager;
+import sg.xueyu.efamily.base.ejb.LoginUserEJB;
+import sg.xueyu.efamily.base.ejb.RoleEJB;
+import sg.xueyu.efamily.dao.RoleDao;
+import sg.xueyu.efamily.dao.UserDao;
+import sg.xueyu.efamily.system.SystemConstants;
+import sg.xueyu.zebra.action.ActionResult;
+import sg.xueyu.zebra.action.ResultContent;
+
+public class BaseAction {
+
+	private Connection mConnection = null;
+	private SessionManager mSessionManager = null;
+
+	public BaseAction() throws Exception {
+		mConnection = new DataSource().getConnection();
+		mSessionManager = SessionManager.getInstance();
+	}
+
+	protected Connection getConnection() {
+		return mConnection;
+	}
+
+	protected SessionManager getmSessionManager() {
+		return mSessionManager;
+	}
+
+	protected ActionResult credentialAuthentication(HttpServletRequest req) throws Exception {
+		ResultContent resultContent = null;
+		ActionResult actionResult = null;
+		UserDao userDao = new UserDao(mConnection);
+		RoleDao roleDao = new RoleDao(mConnection);
+
+		// Session UserId is null
+		String sessionUserId = mSessionManager.getCredentials(req.getSession());
+		if (sessionUserId == null) {
+			resultContent = new ResultContent(SystemConstants.URL_LOGIN, null);
+			actionResult = new ActionResult(resultContent);
+
+			return actionResult;
+		}
+		// Session User is not exist in DB
+		LoginUserEJB sessionUser = userDao.getUser(sessionUserId);
+		if (sessionUser == null) {
+			resultContent = new ResultContent(SystemConstants.URL_LOGIN, null);
+			actionResult = new ActionResult(resultContent);
+
+			return actionResult;
+		}
+		// Role of session User is not exist in DB
+		RoleEJB sessionRole = roleDao.getRole(sessionUser.getRoleId());
+		if (sessionRole == null) {
+			resultContent = new ResultContent(SystemConstants.URL_LOGIN, null);
+			actionResult = new ActionResult(resultContent);
+
+			return actionResult;
+		}
+
+		return null;
+	}
+
+}
--- tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/action/HomeAction.java
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/action/HomeAction.java
@@ -1,17 +1,8 @@
 package sg.xueyu.efamily.action;
 
-import java.sql.Connection;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import sg.xueyu.dbhandler.util.DBUtils;
-import sg.xueyu.efamily.base.DataSource;
-import sg.xueyu.efamily.base.SessionManager;
-import sg.xueyu.efamily.base.ejb.LoginUserEJB;
-import sg.xueyu.efamily.base.ejb.RoleEJB;
-import sg.xueyu.efamily.dao.RoleDao;
-import sg.xueyu.efamily.dao.UserDao;
 import sg.xueyu.efamily.system.SystemConstants;
 import sg.xueyu.efamily.system.SystemLogger;
 import sg.xueyu.zebra.action.Action;
@@ -19,52 +10,27 @@ import sg.xueyu.zebra.action.ActionResult;
 import sg.xueyu.zebra.action.ResultContent;
 import sg.xueyu.zebra.action.ResultType;
 
-public class HomeAction implements Action {
+public class HomeAction extends BaseAction implements Action {
+
+	public HomeAction() throws Exception {
+		super();
+	}
 
 	@Override
 	public ActionResult execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		ResultContent resultContent = null;
 		ActionResult actionResult = null;
 
-		SessionManager sessionManager = SessionManager.getInstance();
-		Connection conn = null;
-		UserDao userDao = null;
-		RoleDao roleDao = null;
-
 		try {
-			conn = new DataSource().getConnection();
-			userDao = new UserDao(conn);
-			roleDao = new RoleDao(conn);
-
-			// Session UserId is null
-			String sessionUserId = sessionManager.getCredentials(req.getSession());
-			if (sessionUserId == null) {
-				resultContent = new ResultContent(SystemConstants.URL_LOGIN, null);
-				actionResult = new ActionResult(resultContent);
-
-				return actionResult;
+			ActionResult authResult = credentialAuthentication(req);
+			if (authResult != null) {
+				return authResult;
 			}
-			// Session User is not exist in DB
-			LoginUserEJB sessionUser = userDao.getUser(sessionUserId);
-			if (sessionUser == null) {
-				resultContent = new ResultContent(SystemConstants.URL_LOGIN, null);
-				actionResult = new ActionResult(resultContent);
-
-				return actionResult;
-			}
-			// Role of session User is not exist in DB
-			RoleEJB sessionRole = roleDao.getRole(sessionUser.getRoleId());
-			if (sessionRole == null) {
-				resultContent = new ResultContent(SystemConstants.URL_LOGIN, null);
-				actionResult = new ActionResult(resultContent);
-
-				return actionResult;
-			}
-
+			
 			// Perform to forward to Home.jsp
 			resultContent = new ResultContent(SystemConstants.URL_HOME, null);
 			actionResult = new ActionResult(resultContent);
-			
+
 			return actionResult;
 		} catch (Exception e) {
 			SystemLogger.error(e);
@@ -72,10 +38,8 @@ public class HomeAction implements Action {
 			resp.setStatus(500);
 			resultContent = new ResultContent(null, "UnHandled Exception Occurred!!!");
 			actionResult = new ActionResult(resultContent, ResultType.Ajax);
-		} finally {
-			DBUtils.closeConnection(conn);
 		}
-		
+
 		return actionResult;
 	}
 
