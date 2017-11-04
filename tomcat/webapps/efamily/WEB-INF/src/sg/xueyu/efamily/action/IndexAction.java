8343229f46c9f8e125278112b92e510008a2df8e
MAC <xueyublue@gmail.com>
2017-11-04 22:45:52 +0800

    -

--- tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/action/IndexAction.java
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/action/IndexAction.java
@@ -6,16 +6,13 @@ import javax.servlet.http.HttpServletResponse;
 import sg.xueyu.efamily.system.SystemConstants;
 import sg.xueyu.zebra.action.Action;
 import sg.xueyu.zebra.action.ActionResult;
-import sg.xueyu.zebra.action.ResultContent;
+import sg.xueyu.zebra.controller.ActionController;
 
 public class IndexAction implements Action {
 
 	@Override
 	public ActionResult execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		ResultContent resultContent = new ResultContent(SystemConstants.URL_LOGIN, null);
-		ActionResult actionResult = new ActionResult(resultContent);
-		
-		return actionResult;
+		return ActionController.buildActionResultWithURL(SystemConstants.URL_LOGIN);
 	}
 
 }
--- tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/action/LoginAction.java
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/action/LoginAction.java
@@ -6,16 +6,13 @@ import javax.servlet.http.HttpServletResponse;
 import sg.xueyu.efamily.system.SystemConstants;
 import sg.xueyu.zebra.action.Action;
 import sg.xueyu.zebra.action.ActionResult;
-import sg.xueyu.zebra.action.ResultContent;
+import sg.xueyu.zebra.controller.ActionController;
 
 public class LoginAction implements Action {
 	
 	@Override
-	public ActionResult execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		ResultContent resultContent = new ResultContent(SystemConstants.URL_LOGIN, null);
-		ActionResult result = new ActionResult(resultContent);
-		
-		return result;
+	public ActionResult execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {	
+		return ActionController.buildActionResultWithURL(SystemConstants.URL_LOGIN);
 	}
 
 }
