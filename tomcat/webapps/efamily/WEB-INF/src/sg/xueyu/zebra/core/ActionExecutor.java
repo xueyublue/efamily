7e0fceeb4d19e02aa89504f89d7b1553c221b2b3
WIN-Dell <xueyublue@gmail.com>
2017-11-17 11:05:42 +0800

    -

--- tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/zebra/core/ActionExecutor.java
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/zebra/core/ActionExecutor.java
@@ -1,5 +1,18 @@
 package sg.xueyu.zebra.core;
 
+import java.util.List;
+
 public class ActionExecutor {
+
+	private List<Action> mActionList = null;
+
+	public ActionExecutor(List<Action> actions) {
+		this.mActionList = actions;
+	}
+	
+	public boolean execute() {
+
+		return false;
+	}
 
 }
