90c7a0d23ebe1752888f16744d3bb5c9d30b2134
WIN-Dell <xueyublue@gmail.com>
2017-11-17 16:54:34 +0800

    -

--- tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/system/CommonMethod.java
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/efamily/system/CommonMethod.java
@@ -3,36 +3,7 @@ package sg.xueyu.efamily.system;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import javax.servlet.http.HttpSession;
-
 public class CommonMethod {
-
-//	public static final String getSessionCredentials(HttpSession session) {
-//		Object userId = session.getAttribute("userId");
-//		if (userId == null) {
-//			return null;
-//		}
-//		return userId.toString();
-//	}
-//
-//	public static final boolean checkSessionCredentials(HttpSession session) {
-//		Object userId = session.getAttribute("userId");
-//		Object userName = session.getAttribute("userName");
-//		if (userId != null && userName != null) {
-//			return true;
-//		}
-//		return false;
-//	}
-//
-//	public static final void setSessionCredentials(HttpSession session, String userId, String userName) {
-//		session.setAttribute("userId", userId);
-//		session.setAttribute("userName", userName);
-//	}
-//
-//	public static final void removeSessionCredentials(HttpSession session) {
-//		session.removeAttribute("userId");
-//		session.removeAttribute("userName");
-//	}
 
 	public static boolean isVoid(Object obj) {
 		if (obj == null || obj.toString() == null) {
