93686463b15050d18ee2ded34e2ef506b62faa49
WIN-Dell <xueyublue@gmail.com>
2017-11-21 08:20:25 +0800

    -

--- tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/zebra/RootController.java
+++ tomcat/webapps/efamily/WEB-INF/src/sg/xueyu/zebra/RootController.java
@@ -2,8 +2,6 @@ package sg.xueyu.zebra;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -14,19 +12,16 @@ import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 import sg.xueyu.zebra.action.ActionResult;
 import sg.xueyu.zebra.action.ResultContent;
 import sg.xueyu.zebra.action.ResultType;
-import sg.xueyu.zebra.action.Uploadable;
 import sg.xueyu.zebra.annotation.Method.RequestMethod;
-import sg.xueyu.zebra.core.Action;
 import sg.xueyu.zebra.core.ActionContainer;
+import sg.xueyu.zebra.core.ActionExecutor;
 import sg.xueyu.zebra.core.ActionScanner;
 import sg.xueyu.zebra.core.ConfigurationContainer;
 import sg.xueyu.zebra.core.ConfigurationScanner;
-import sg.xueyu.zebra.core.DataBinding;
 import sg.xueyu.zebra.core.PackageScanner;
 
 @MultipartConfig
@@ -136,6 +131,9 @@ public class RootController extends HttpServlet {
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String contextPath = req.getContextPath() + "/";
 		String servletPath = req.getServletPath();
+
+		// Create action executor
+		ActionExecutor actionExecutor = new ActionExecutor(req, resp, mActionContainer);
 		
 		// Get request path
 		// Remove suffix of request URL
@@ -151,48 +149,7 @@ public class RootController extends HttpServlet {
 		RequestMethod requestMethod = getRequestMethod(req);
 					
 		try {
-			
-			// Find action that matched request URL and HTTP request method
-			Action action = mActionContainer.find(requestPath, requestMethod);
-			if (action == null) {
-				throw new Exception("No matched action class found under package: " + mPackageScanList);
-			}
-			
-			// Get action class from Action object
-			Class<?> actionClass = action.getActionClass();
-			
-			// Initialize action class instance
-			Object actionInstance = null;
-			Constructor<?>[] constructor = actionClass.getConstructors();
-			
-			for (Constructor<?> cons : constructor) {
-				Class<?>[] types = cons.getParameterTypes();
-				// If constructor do not have any parameters inside
-				// Then will be using the default constructor to initialize action class
-				if(types.length == 0) {
-					actionInstance = cons.newInstance();	
-				}
-				// If constructor have 2 parameters inside and parameter type is HttpServletRequest/HttpServletResponse
-				// Then will be using it to initialize action class
-				else if (types.length == 2
-						&& types[0].equals(HttpServletRequest.class)
-						&& types[1].equals(HttpServletResponse.class)) {
-					actionInstance = cons.newInstance(req, resp);
-				} 
-				// If others then set action instance to null
-				else {
-					actionInstance = null;
-				}
-			}
-			
-			Method actionMethod = action.getMethod();
-			
-			// Inject request data to Action method
-			DataBinding dataBinding = new DataBinding(req);
-			Object[] values = dataBinding.bindToMethod(actionMethod);
-			
-			// Invoke action
-			ActionResult actionResult = (ActionResult) actionMethod.invoke(actionInstance, values);
+			ActionResult actionResult = actionExecutor.execute(requestPath, requestMethod);
 			
 //			// Handle file upload Action
 //			// TODO: Implement file upload action
